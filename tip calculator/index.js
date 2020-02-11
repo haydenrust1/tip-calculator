b72f71da0e506ec9f71f139b16433a17310bf0de
Hayden Rust <48568887+haydenrust1@users.noreply.github.com>
2020-02-11 08:50:17 -0800

    Update index.js

--- tip calculator/index.js
+++ tip calculator/index.js
@@ -1,27 +1,67 @@
-//UI Controller
-
-const getInput = function() {
+//Calculator Controller
+const listen = function() {
     
-    const input = [];
-    const bill = input.push(document.getElementById('bill-input'));
-    const tip = input.push(document.getElementById('tip-input'));
-    const guest = input.push(document.getElementById('guests-input'));
-    // console.log(input);
+    let input = [];
+
+    input.push(parseFloat((document.getElementById('bill-input').value)));  //user-entered bill amount
+    input.push(parseFloat(((document.getElementById('tip-input').value))/100));  //user-entered tip %
+    input.push(parseFloat((document.getElementById('guests-input').value)));  //user-entered guests present 
+    
+    function tipOut() {
+        if (input[2] > 1) {
+            
+            let tipPerPerson = parseFloat(((input[0] * input[1]).toFixed(2)) / input[2]); //bill * tip%
+            let billPerPerson = parseFloat((input[0]) / input[2]);
+            
+            let totalPerPerson = (tipPerPerson + billPerPerson); //tip amount + bill
+            
+            document.getElementById('tip_result').innerHTML = `$${tipPerPerson}`;
+            document.getElementById('total-bill').innerHTML = `$${totalPerPerson}`;
+
+            /*********************************  Console Testing **************************************************
+            console.log(`The Bill is ${input[0]}. The Tip is ${input[1]}. The amount of guests are ${input[2]}`);
+            console.log(`the tip per person is ${tipPerPerson}`);
+            console.log(`the bill per person is ${billPerPerson}`);
+            console.log(`the total per person is ${totalPerPerson}`);  
+            ******************************************************************************************************/
+        }else {
+            
+            let tipOwed = parseFloat((input[0] * input[1]).toFixed(2)); //bill * tip%
+            let total = tipOwed + input[0]; //tip amount + bill
+            
+            document.getElementById('tip_result').innerHTML = `$${tipOwed}`;
+            document.getElementById('total-bill').innerHTML = `$${total}`;
+
+            /*********************************  Console Testing **************************************************
+            console.log(`The Bill is ${input[0]}. The Tip is ${input[1]}. The amount of guests are ${input[2]}`);
+            console.log(`the tip owed is ${tipOwed}`);
+            console.log(`the total bill is ${total}`);  
+            ******************************************************************************************************/
+       }
+        
+  
+    }
+    return tipOut();
     
 };
 
-const eventListener = function() {
+//UI Controller
+const eventListener = function() {  
     
-    const listen = document.querySelector('.button').addEventListener('click', getInput);
-    console.log(listen);
+    document.getElementById('click').addEventListener('click', listen);  //mouse-click event
+    
+    document.addEventListener('keypress', function(event) {  //enter-key event
+        
+        if(event.keyCode === 13 || event.which === 13) {
+            listen();
+        }
+    });
+    
+    addEventListener('submit', function handleForm(event) {  //stops form from refreshing page
+        
+        document.getElementById("form_calculator");
+        event.preventDefault(); 
+    });
 };
 
 eventListener();
-
-
-//Calculator Controller
-
-// const bill = document.querySelector('#bill-input').value;
-// console.log(bill);
-
-
